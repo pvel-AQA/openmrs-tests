24e9cceabbaed69617bd0458b24016c161f7ab2e
PavelV <202444749+pvel-AQA@users.noreply.github.com>
2026-05-02 13:04:06 +0300

    fix syntax

--- src/main/java/api/models/CreatePersonResponse.java
+++ src/main/java/api/models/CreatePersonResponse.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class CreatePersonResponse extends BaseModel{
+public class CreatePersonResponse extends BaseModel {
     private String uuid;
     private String display;
     private String gender;
