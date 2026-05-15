c3a1d2536fdf234db9a1afcb641ccadabbfc05a0
nkashyrina <nkashyrina@gmail.com>
2026-05-15 10:50:26 -0500

    removed doubled header on pages

--- src/main/java/ui/pages/LoginPage.java
+++ src/main/java/ui/pages/LoginPage.java
@@ -9,7 +9,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Getter
 public class LoginPage extends BasePage<LoginPage> {
     @Override
     public String url() {
