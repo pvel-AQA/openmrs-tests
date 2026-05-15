5adf29a7aa01758c6112d765eef0d2f446fa66bc
nkashyrina <nkashyrina@gmail.com>
2026-05-15 09:43:12 -0500

    removed "outpatient" dublicate from constants.java

--- src/main/java/api/constants/Constants.java
+++ src/main/java/api/constants/Constants.java
@@ -4,7 +4,6 @@ public final class Constants {
     public static String[] personFieldsToBeGenerated = new String[]{"gender", "birthdate", "birthdateEstimated",
             "dead", "addresses", "attributes"};
     public static String[] nameFieldsToBeGenerated = new String[]{"givenName", "middleName", "familyName"};
-    public static String ClinicNameToGetLocationUuid = "Outpatient";
     public static final boolean PREFERRED_IDENTIFIER_TRUE = true;
 
     private Constants() {
