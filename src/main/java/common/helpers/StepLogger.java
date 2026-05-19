1bae4d65584a103a21fb3b874956af06860a09d0
PavelV <202444749+pvel-AQA@users.noreply.github.com>
2026-05-19 20:23:43 +0300

    add Allure dependencies; Add allure steps to long CreatePatient tests

---
+++ src/main/java/common/helpers/StepLogger.java
@@ -0,0 +1,28 @@
+package common.helpers;
+
+import io.qameta.allure.Allure;
+
+public final class StepLogger {
+    private StepLogger() { }
+
+    @FunctionalInterface
+    public interface ThrowableRunnable<T> {
+        T run() throws Throwable;
+    }
+
+    @FunctionalInterface
+    public interface ThrowableVoidRunnable {
+        void run() throws Throwable;
+    }
+
+    public static <T> T log(String title, ThrowableRunnable<T> runnable) {
+        return Allure.step(title, runnable::run);
+    }
+
+    public static void log(String title, ThrowableVoidRunnable runnable) {
+        Allure.step(title, () -> {
+            runnable.run();
+            return null;
+        });
+    }
+}
