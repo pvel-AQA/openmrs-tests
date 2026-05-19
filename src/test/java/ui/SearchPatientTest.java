a069feb13a1740ecc7922fcaa2132b6e2d7c9527
nkashyrina <nkashyrina@gmail.com>
2026-05-19 11:59:56 -0500

    reacted on code review notes

--- src/test/java/ui/SearchPatientTest.java
+++ src/test/java/ui/SearchPatientTest.java
@@ -39,7 +39,7 @@ public class SearchPatientTest extends BaseUiTest {
     void searchDropdownShouldShowCorrectResultsTest() {
 
         String generatedString = RandomDataGenerator.randomString(7);
-        List<String> createdUuids createdUuids = AdminSteps.createPatientsForSearch(4, true, generatedString);
+        List<String> createdUuids = AdminSteps.createPatientsForSearch(4, true, generatedString);
         String searchText = generatedString.substring(0, 4);
 
         new PickLocationPage().open().pickOutpatientLocationAndConfirm();
