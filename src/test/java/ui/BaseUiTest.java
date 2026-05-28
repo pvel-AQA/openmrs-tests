85e6c7ba45da1d64845aa79f218536323c344175
nkashyrina <nkashyrina@gmail.com>
2026-05-28 15:57:45 -0500

    CI/CD pipeline setup: added BeforeEach to BaseUiTest

--- src/test/java/ui/BaseUiTest.java
+++ src/test/java/ui/BaseUiTest.java
@@ -7,9 +7,12 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import common.extensions.AdminSessionExtension;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @ExtendWith(AdminSessionExtension.class)
 public class BaseUiTest extends BaseTest {
@@ -24,5 +27,16 @@ public class BaseUiTest extends BaseTest {
 
         Configuration.browserCapabilities.setCapability("selenoid:options",
                 Map.of("enableVNC", true, "enableLog", true));
+    }
+
+    @BeforeEach
+    public void setUniqueUserDataDir() {
+        // Each test session gets a unique Chrome user-data-dir so concurrent or
+        // back-to-back sessions don't collide on the default profile path.
+        String uniqueDir = "/tmp/chrome-" + UUID.randomUUID();
+        Configuration.browserCapabilities.setCapability(
+                "goog:chromeOptions",
+                Map.of("args", List.of("--user-data-dir=" + uniqueDir))
+        );
     }
 }
